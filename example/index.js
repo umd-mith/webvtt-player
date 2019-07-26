2de9405052f991389fee2a28e6bf40cad6903234
Ed Summers <ehs@pobox.com>
2019-07-26 17:56:41 -0400

    cleaned up example

--- example/index.js
+++ example/index.js
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Player } from '../src/'
 
-//     transcript='http://mith-uta.s3-website-us-east-1.amazonaws.com/data/transcripts/cpb-aacip-500-m03xxr0v/cpb-aacip-500-m03xxr0v.vtt' 
-
-
 ReactDOM.render(
   <Player
-    audio='http://mith-uta.s3-website-us-east-1.amazonaws.com/data/audio/cpb-aacip-500-m03xxr0v.mp3'
-    transcript='http://mith-uta.s3-website-us-east-1.amazonaws.com/data/transcripts/cpb-aacip-500-m03xxr0v/cpb-aacip-500-m03xxr0v.vtt' />,
+    audio="data/audio.mp3"
+    transcript="data/transcript.vtt"
   document.getElementById('root')
 )
